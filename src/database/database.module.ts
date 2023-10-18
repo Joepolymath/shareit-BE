7425b34049bbc97c36824432b0acc5399a15cb68
Joshua_Ajagbe <joshua@equally.ai>
2023-10-18 03:18:16 +0100

    postgres db connection

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import { createConnection, ConnectionCreatedEvent } from 'typeorm';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  const connection = await createConnection();
+
+  connection.isInitialized &&
+    console.log('Database connection established successfully');
+
   await app.listen(3000);
 }
 bootstrap();
